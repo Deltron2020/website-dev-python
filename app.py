5713c857c5edf4b8933d5c2789789f47f64322e7
Deltron2020 <45208554+Deltron2020@users.noreply.github.com>
2023-12-11 21:06:36 +0000

    Added JSON & jobitem.html

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': '$80000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Miami',
+    'salary': '$100000'
+  },
+  {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'C++ Developer',
+    'location': 'Houston',
+    'salary': '$90000'
+  }
+  
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', 
+                         jobs=JOBS,
+                         company_name='Property Appraiser')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 print(__name__)
